4a2f1673ec89ad98d5be999cea5b74c7dd266b90
Pavel Kozlov <sovvvest@yandex.ru>
2018-08-14 11:41:28 +0400

    Initial commit

---
+++ fusioninventory_agent_install.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+#Script for installing fusioninventory agent on Ubuntu/Debian
+
+cd /tmp
+
+# Install dependencies:
+apt -y install wget dmidecode hwdata ucf hdparm perl libuniversal-require-perl \
+libwww-perl libparse-edid-perl libproc-daemon-perl libfile-which-perl libxml-treepp-perl \
+libyaml-perl libnet-cups-perl libnet-ip-perl libdigest-sha-perl libsocket-getaddrinfo-perl \
+libtext-template-perl nmap libnet-snmp-perl libcrypt-des-perl libnet-nbname-perl
+
+# Download and install fusioninventory aget
+
+wget http://debian.fusioninventory.org/downloads/fusioninventory-agent_2.4-2_all.deb
+
+dpkg -i fusioninventory-agent_2.4-2_all.deb
+
+# Configuring agent
+
+sed -i 14d /etc/fusioninventory/agent.cfg
+sed -i "14i server = http://glpi.gusadev.com/plugins/fusioninventory" /etc/fusioninventory/agent.cfg
+
+sed -i 32d /etc/fusioninventory/agent.cfg
+sed -i "32i delaytime = 60" /etc/fusioninventory/agent.cfg
+
+service fusioninventory-agent restart
